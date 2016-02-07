35120c8bb6baa10d05ed41abbb1b8e316816aca1
Vikas Reddy <vikasreddy@Vikass-Mac.local>
2016-02-07 13:48:42 -0700

    Added all files

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -10,8 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapLbl: UILabel!
+    
+    //Properties
+    var currentTaps = 0
+    var maxTaps = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.logoImage.hidden = false
+        self.howManyTapsTxt.hidden = false
+        self.playBtn.hidden = false
+        
+        self.howManyTapsTxt.text = ""
+        self.tapBtn.hidden = true
+        self.tapLbl.hidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +38,39 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!) {
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            
+            logoImage.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            
+            tapBtn.hidden = false
+            tapLbl.hidden = false
+            
+            updateTapsLabel()
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+        }
+    }
+    
+    @IBAction func onTapBtnPressed(sender: UIButton!) {
+        currentTaps += 1
+        updateTapsLabel()
+        if  isGameOver() {
+            viewDidLoad()
+        }
+    }
+    
+    func isGameOver() -> Bool {
+        return currentTaps >= maxTaps + 1
+    }
+    func updateTapsLabel() {
+        tapLbl.text = "\(currentTaps) Taps"
 
-
+    }
 }
 
